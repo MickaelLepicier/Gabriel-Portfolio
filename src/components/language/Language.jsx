6ae179c1d8e6dc0ca680c143e3915cb49a5d4611
MickaelLepicier <mickael233@gmail.com>
2025-10-16 09:27:25 +0300

    chore: update language

--- src/components/language/Language.jsx
+++ src/components/language/Language.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Language() {
     const languages = [
-        {code:'Hr', name:'Hebrew', level:'Fluent'},
+        {code:'Hr', name:'Hebrew', level:'Native'},
         {code:'En', name:'English', level:'Fluent'},
         // {code:'Fr', name:'French', level:'Basic'},
     ]
